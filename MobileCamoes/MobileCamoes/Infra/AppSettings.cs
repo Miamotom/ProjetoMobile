63e2790222717d8abd801f79e84cf75a87017b4d
Nilton Andrade <nilton_junior_nilto@hotmail.com>
2020-05-04 16:59:49 -0300

    Initial Comit V1.0

---
+++ MobileCamoes/MobileCamoes/Infra/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace MobileCamoes.Infra
+{
+    public class AppSettings
+    {
+        public const string ApiUrl = "https://api.themoviedb.org/3";
+        public const string ApiKey = "d789470d38cce2276363d13b4c7ad26c";
+        public const string ApiImageBaseUrl = "https://image.tmdb.org/t/p/w500/";
+
+        public static string OfflineDataBaseLiteDBPath { get; set; }
+        public static string OfflineDataBaseSQLitePath { get; set; }
+        public static string OfflineFileSystemPath { get; set; }
+    }
+}
